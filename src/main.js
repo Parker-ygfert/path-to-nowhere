fdd1b1cdcfb3ed6490392b7e358bc14e49ec2c27
Parker <ygfert@gmail.com>
2023-01-31 02:53:58 +0800

    WIP

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/styles/reset.css' 
+import './assets/styles/style.sass' 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import App from './App.vue'
