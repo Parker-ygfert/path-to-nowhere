2db22fed768c68d32c4b8155e72a2b5412a09ebe
Parker <ygfert@gmail.com>
2023-02-02 03:35:37 +0800

    add lazyload package

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import './assets/styles/style.sass'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import App from './App.vue'
+import lazyloadImgInit, { LazyloadImg } from 'vue3-lazyload-img'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(lazyloadImgInit, { retryLoad: 0 })
+   .mount('#app')
