2f028f1c2f96879abbd50967403e5266de9aed52
Parker <ygfert@gmail.com>
2023-03-04 14:47:23 +0800

    add OwO crimebrands

--- src/components/CrimeBrands.vue
+++ src/components/CrimeBrands.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="character-img position-relative">
-    <Character job="endura" :character="currentCharacter" />
-  </div>
+  <div class="d-flex">
+    <div class="character-img position-relative me-3">
+      <Character job="endura" :character="currentCharacter" />
+    </div>
 
-  <div v-for="cb in currentCharacter.crimebrands" class="font-20">
-    <header class="font-30">{{ cb.name }}</header>
-    <div class="d-flex">
-      <div v-for="position in ['first', 'second', 'third']"
-           :set="cbName = cb[position]"
-           class="crimebrands-img position-relative flex-shrink-0 border border-dark">
-        <img v-if="cbName"
-             :src="getImageUrl(`crimebrands/${cbRank(cbName)}.png`)" alt=""
-             class="position-absolute">
-        <img v-if="cbName"
-             :src="getImageUrl(`crimebrands/${cbName}.png`)" alt=""
-             class="w-100 h-100">
-        <span class="crimebrands-name position-absolute font-16 fw-bold text-white">
-          {{ $t(cbName) }}
-        </span>
+    <div class="cb-box overflow-auto">
+      <div v-for="cb in currentCharacter.crimebrands"
+           class="mb-1 font-20">
+        <header class="font-30">{{ $t(cb.name) }}</header>
+
+        <div class="d-flex">
+          <div v-for="position in ['first', 'second', 'third']"
+               :set="cbName = cb[position]"
+               class="crimebrands-img position-relative flex-shrink-0 border border-dark">
+            <img v-if="cbName"
+                 :src="getImageUrl(`crimebrands/${cbRank(cbName)}.png`)" alt=""
+                 class="position-absolute">
+            <img v-if="cbName"
+                 :src="getImageUrl(`crimebrands/${cbName}.png`)" alt=""
+                 class="w-100 h-100">
+            <span class="crimebrands-name position-absolute font-16 fw-bold text-white">
+              {{ $t(cbName) }}
+            </span>
+          </div>
+        </div>
+
+        <div>{{ cb.description }}</div>
       </div>
-
-      <div>{{ cb.description }}</div>
     </div>
   </div>
 </template>
@@ -29,13 +35,15 @@
 .character-img
   width: 180px
   height: 270px
-.crimebrands-img
-  width: 100px
-  height: 100px
-  .crimebrands-name
-    right: 4px
-    bottom: 0
-    text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000
+.cb-box
+  height: 80vh
+  .crimebrands-img
+    width: 100px
+    height: 100px
+    .crimebrands-name
+      right: 4px
+      bottom: 0
+      text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000
 </style>
 
 <script setup>
