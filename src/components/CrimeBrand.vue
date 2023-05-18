6b673bcbeaae877f31f3806ca163534d7e689790
Parker <ygfert@gmail.com>
2023-05-18 18:46:09 +0800

    update crimebrans recommends

--- src/components/CrimeBrand.vue
+++ src/components/CrimeBrand.vue
@@ -4,7 +4,7 @@
     —— {{ $t('crimebrands_recommends') }} ——
     <div class="font-12 text-danger">紅色表示通用性較高</div>
   </div>
-  <div class="cb-formula overflow-scroll">
+  <div class="cb-formula">
     <div
       v-for="cb in sinner?.crimebrands"
       class="cb-set my-1 font-18"
@@ -37,13 +37,15 @@
             class="w-100 h-100"
           >
           <span class="crimebrands-name position-absolute font-14 fw-bold text-white">
-            {{ $t(cbName) }}
+            {{ $t(`crimebrands.${cbName}`) }}
           </span>
           <div class="rank-deco" :class="'deco-' + cbRank(cbName)"></div>
         </div>
       </div>
-      <div class="text-secondary">
-        {{ cb.description }}
+      <div
+        class="text-secondary"
+        v-html="cb.description"
+      >
       </div>
     </div>
   </div>
@@ -53,36 +55,32 @@
 <style lang="sass" scoped>
 @import '@/assets/styles/_rwd.sass'
 
-.cb-formula
-  max-height: calc(100vh - 228px)
+.cb-set
+  width: 300px !important
   @include tablet
-    max-height: fit-content
-  .cb-set
-    width: 300px !important
-    @include tablet
-      width: fit-content
-      margin: auto
-    .crimebrands-img
-      width: 100px
-      height: 100px
-      .crimebrands-name
-        right: 4px
-        bottom: 1px
-        text-shadow: -1px 0 2px #000, 0 1px 2px #000, 1px 0 2px #000, 0 -1px 2px #000
-      .rank-deco
-        display: inline-block
-        bottom: 0
-        right: 0
-        position: absolute
-        pointer-events: none
-        width: 98px
-        height: 5px
-      .deco-s
-        background-image: linear-gradient(rgba(255,0,0,0),rgba(255,220,93,1) 75% )
-      .deco-a
-        background-image: linear-gradient(rgba(255,0,0,0),rgba(241,174,255,1) 75% )
-      .deco-b
-        background-image: linear-gradient(rgba(255,0,0,0),rgba(117,171,240,1) 75% )
+    width: fit-content
+    margin: auto
+  .crimebrands-img
+    width: 100px
+    height: 100px
+    .crimebrands-name
+      right: 4px
+      bottom: 1px
+      text-shadow: -1px 0 2px #000, 0 1px 2px #000, 1px 0 2px #000, 0 -1px 2px #000
+    .rank-deco
+      display: inline-block
+      bottom: 0
+      right: 0
+      position: absolute
+      pointer-events: none
+      width: 98px
+      height: 5px
+    .deco-s
+      background-image: linear-gradient(rgba(255,0,0,0),rgba(255,220,93,1) 75% )
+    .deco-a
+      background-image: linear-gradient(rgba(255,0,0,0),rgba(241,174,255,1) 75% )
+    .deco-b
+      background-image: linear-gradient(rgba(255,0,0,0),rgba(117,171,240,1) 75% )
 </style>
 
 <script setup>
